73eafb972504f4aaadfa6da89f458a742cdc21a8
Justin Dority <justindority@gmail.com>
2022-07-18 15:50:36 -0500

    initial commit

---
+++ scripts/Employees.js
@@ -0,0 +1,38 @@
+import { getEmployees, getOrders } from "./database.js"
+
+const employees = getEmployees()
+const orders = getOrders()
+
+export const Employees = () => {
+    let html = "<ul>"
+
+    for (const employee of employees) {
+        html += `<li id="employee--${employee.id}">${employee.name}</li>`
+    }
+
+    html += "</ul>"
+
+    return html
+}
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) {
+            const [,employeeId] = itemClicked.id.split("--")
+
+            for (const employee of employees) {
+                if (employee.id === parseInt(employeeId)) {
+                    let count = 0
+                    for(const order of orders){
+                        if(employee.id === order.employeeId){
+                            count++
+                        }
+                    }
+                    window.alert(`${employee.name} has sold ${count} products.`)
+                }
+            }
+        }
+    }
+)
